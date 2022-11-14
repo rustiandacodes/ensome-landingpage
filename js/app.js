c6c4976508d0b3a7eada6b0fff50d4f73045a4f8
rustiandacode <rustiandazen09@gmail.com>
2022-11-14 20:01:57 +0700

    adding dropdown feature on navbar responsive screen

--- js/app.js
+++ js/app.js
@@ -10,3 +10,20 @@ menuToggle.addEventListener('click', () => {
 menuToggleRemove.addEventListener('click', () => {
   nav.classList.remove('slide');
 });
+
+const pages = document.querySelector('#pages');
+const pagesContent = document.querySelector('.dropdown-responsive-content');
+
+pages.classList.add('fa-chevron-down');
+
+pages.addEventListener('click', () => {
+  if (pages.classList.contains('fa-chevron-up')) {
+    pages.classList.remove('fa-chevron-up');
+    pages.classList.toggle('fa-chevron-down');
+    pagesContent.style.display = 'none';
+  } else if (pages.classList.contains('fa-chevron-down')) {
+    pages.classList.remove('fa-chevron-down');
+    pages.classList.toggle('fa-chevron-up');
+    pagesContent.style.display = 'block';
+  }
+});
